6a4bac6f2db7902152e115687c38c555b45cae25
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-12-11 14:54:59 -0500

    prompts for title

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
+// Inluded rquire and inquirer packeages
+const fs = require('fs');
+const inquirer = require('inquirer');
+const lib = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your title of readme?', "Enter your Description"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(questions) {
+    // prompting user
+    inquirer.prompt([{
+            // prompting user for title
+            type: 'input',
+            name: 'title',
+            message: questions[0]
+        }, {
+            // prompting user for description
+            type: 'input',
+            name: 'description',
+            message: questions[1]
+        }])
+        .then((answers) => {
+            console.log(answers)
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+
+}
 
 // Function call to initialize app
-init();
+init(questions);
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+
+const inquirer = require("inquirer");
+// Title Question
+let renderTitle = (question) => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'title',
+        message: question
+    }]).then(answers => {
+        console.log(answers.title);
+    })
+}
+
+let renderDescription = (question) => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'description',
+        message: question
+    }]).then(answers => {
+        console.log(answers.title + " " +
+            answers.description);
+    })
+}
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+
+
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
@@ -12,9 +40,11 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title}
 
-`;
+    `;
+
+
 }
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown, renderTitle, renderDescription };
